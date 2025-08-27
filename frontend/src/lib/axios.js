7d2f28c8cdcd2b01e4db627042563b542a023027
Dibyanshujha9 <jhadibyanshu46@gmail.com>
2025-08-27 22:26:54 +0530

    Deployment

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chat-app-backend-ka8x.onrender.com/api",
+  withCredentials: true,
+});
