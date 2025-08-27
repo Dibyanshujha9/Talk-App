8c4909c7fd7d42683f498470edcccac0a9c2c63a
Dibyanshujha9 <jhadibyanshu46@gmail.com>
2025-08-27 22:57:43 +0530

    For Frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,15 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === "development"
+//       ? "http://localhost:5001/api"
+//       : "https://chat-app-backend-ka8x.onrender.com/api",
+//   withCredentials: true,
+// });
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chat-app-backend-ka8x.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
